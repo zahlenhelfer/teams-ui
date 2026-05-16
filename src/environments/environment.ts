f42431a8d0646f2dbf0d5c0329945752076c1cfc
Marcus Ross <marcus.ross@zahlenhelfer.de>
2026-05-16 17:01:47 -0500

    refactor: remove keycloak configuration from environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,5 @@
 export const environment = {
   production: false,
   // Use proxy path instead of direct URL or in coder use "http://<workspace-name>.coder:<port>" with the port of forward of the api service
-  apiUrl: "http://172.18.255.254:4200",
-  keycloak: {
-    // same as above, but with keycloak forward port
-    url: "http://platform-auth.127.0.0.1.sslip.io",
-    realm: "teams",
-    clientId: "teams-ui",
-  },
+  apiUrl: "http://172.18.255.254:4200"
 };

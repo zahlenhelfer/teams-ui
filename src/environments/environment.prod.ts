ca4802915ba1dfac9c5c58c7412c5b43b31e2072
Marcus Ross <marcus.ross@zahlenhelfer.de>
2026-05-16 16:25:54 -0500

    changed keycloak to kubernetes service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   apiUrl: 'http://teams-api-service:4200', // Kubernetes service name
   keycloak: {
     // same as above, but with keycloak forward port
-    url: "http://platform-auth",
+    url: "http://keycloak-service:8080",
     realm: "teams",
     clientId: "teams-ui",
   },

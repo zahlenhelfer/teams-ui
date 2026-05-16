6b94e80baa6bcac99bcde64f36fc5c0c9991ad2b
Marcus Ross <marcus.ross@zahlenhelfer.de>
2026-05-15 20:16:20 -0500

    chore: changed values in env-files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://teams-api-service:4200' // Kubernetes service name
+  apiUrl: 'http://teams-api-service:4200', // Kubernetes service name
+  keycloak: {
+    // same as above, but with keycloak forward port
+    url: "http://platform-auth",
+    realm: "teams",
+    clientId: "teams-ui",
+  },
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   // Use proxy path instead of direct URL or in coder use "http://<workspace-name>.coder:<port>" with the port of forward of the api service
-  apiUrl: "http://teams-api.127.0.0.1.sslip.io",
+  apiUrl: "http://172.18.255.254:4200",
   keycloak: {
     // same as above, but with keycloak forward port
     url: "http://platform-auth.127.0.0.1.sslip.io",

d2f8745cc9515a05b5d9f7c05ce9c2171d6a27d5
Marcus Ross <marcus.ross@zahlenhelfer.de>
2026-05-16 20:34:48 -0500

    refactor: update API and Keycloak URLs to use service names

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   // Use proxy path instead of direct URL or in coder use "http://<workspace-name>.coder:<port>" with the port of forward of the api service
-  apiUrl: "http://172.18.255.254:4200",
+  apiUrl: "http://teams-api-service:4200",
   keycloak: {
-    url: 'http://platform-auth.chekme.io',
+    url: 'http://keycloak-service:8080',
     realm: 'teams',
     clientId: 'teams-ui',
   },

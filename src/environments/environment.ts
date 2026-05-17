b66495aec5a5b5b06dd916829802de7184171ef3
Marcus Ross <marcus.ross@zahlenhelfer.de>
2026-05-16 20:09:16 -0500

    chore: changes to variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   // Use proxy path instead of direct URL or in coder use "http://<workspace-name>.coder:<port>" with the port of forward of the api service
   apiUrl: "http://172.18.255.254:4200",
   keycloak: {
-    url: 'http://platform-auth.127.0.0.1.sslip.io',
+    url: 'http://platform-auth.chekme.io',
     realm: 'teams',
     clientId: 'teams-ui',
   },
